9017109ab93283230550e296b4638e154fbe8fdc
Kim Bøndergaard <kim@fam-boendergaard.dk>
2025-11-29 18:13:24 +0100

    draw: image: Port to LVGL 9.3
    Name change: img -> image

--- src/lvglpp/draw/image.h
+++ src/lvglpp/draw/image.h
@@ -1,11 +1,12 @@
 /** \file image.h
  *  \brief Header file for C++ wrapper for low-level image-handling
- *  LVGL objects, such as lv_img_decoder_t, lv_img_header_t and lv_img_dsc_t.
+ *  LVGL objects, such as lv_image_decoder_t, lv_image_header_t and lv_image_dsc_t.
  *
  *  Author: Vincent Paeder
  *  License: MIT
  */
 #pragma once
+#include "src/draw/lv_image_decoder_private.h"
 #include "../misc/color.h"
 #include "../misc/area.h"
 #include "../lv_wrapper.h"
@@ -19,29 +20,29 @@ namespace lvgl::draw {
     class ImageHeader;
 
     /** \class ImageDecoder
-     *  \brief Wraps a lv_img_decoder_dsc_t object. This is a base class to decode
+     *  \brief Wraps a lv_image_decoder_dsc_t object. This is a base class to decode
      *  an image format.
      */
-    class ImageDecoder : public PointerWrapper<lv_img_decoder_dsc_t, lv_img_decoder_close> {
+    class ImageDecoder : public PointerWrapper<lv_image_decoder_dsc_t, lv_image_decoder_close> {
     protected:
-        /** \fn virtual lv_res_t info_callback(struct _lv_img_decoder_t * decoder, const void * src, lv_img_header_t * header)
+        /** \fn virtual lv_res_t info_callback(struct _lv_img_decoder_t * decoder, const void * src, lv_image_header_t * header)
          *  \brief Callback to read image info.
          *  \param decoder: pointer to the underlying image decoder instance.
          *  \param src: a pointer to an data buffer or a file name.
          *  \param header: pointer to the recipient image header object.
          *  \returns result code.
          */
-        virtual lv_res_t info_callback(struct _lv_img_decoder_t * decoder, const void * src, lv_img_header_t * header) = 0;
+        virtual lv_res_t info_callback(struct _lv_img_decoder_t * decoder, const void * src, lv_image_header_t * header) = 0;
 
-        /** \fn virtual lv_res_t open_callback(struct _lv_img_decoder_t * decoder, struct _lv_img_decoder_dsc_t * dsc)
+        /** \fn virtual lv_res_t open_callback(struct _lv_img_decoder_t * decoder, lv_image_decoder_dsc_t * dsc)
          *  \brief Callback to open image.
          *  \param decoder: pointer to the underlying image decoder instance.
          *  \param dsc: a pointer to an image decoder descriptor.
          *  \returns result code.
          */
-        virtual lv_res_t open_callback(struct _lv_img_decoder_t * decoder, struct _lv_img_decoder_dsc_t * dsc) = 0;
+        virtual lv_res_t open_callback(struct _lv_img_decoder_t * decoder, lv_image_decoder_dsc_t * dsc) = 0;
 
-        /** \fn virtual lv_res_t read_line_callback(struct _lv_img_decoder_t * decoder, struct _lv_img_decoder_dsc_t * dsc, lv_coord_t x, lv_coord_t y, lv_coord_t len, uint8_t * buf)
+        /** \fn virtual lv_res_t read_line_callback(struct _lv_img_decoder_t * decoder, lv_image_decoder_dsc_t * dsc, lv_coord_t x, lv_coord_t y, lv_coord_t len, uint8_t * buf)
          *  \brief Callback to read the image.
          *  \param decoder: pointer to the underlying image decoder instance.
          *  \param dsc: a pointer to an image decoder descriptor.
@@ -51,33 +52,25 @@ namespace lvgl::draw {
          *  \param buf: pointer to the recipient buffer.
          *  \returns result code.
          */
-        virtual lv_res_t read_line_callback(struct _lv_img_decoder_t * decoder, struct _lv_img_decoder_dsc_t * dsc, lv_coord_t x, lv_coord_t y, lv_coord_t len, uint8_t * buf) = 0;
+        virtual lv_res_t read_line_callback(struct _lv_img_decoder_t * decoder, lv_image_decoder_dsc_t * dsc, lv_coord_t x, lv_coord_t y, lv_coord_t len, uint8_t * buf) = 0;
 
-        /** \fn virtual lv_res_t close_callback(struct _lv_img_decoder_t * decoder, struct _lv_img_decoder_dsc_t * dsc)
+        /** \fn virtual lv_res_t close_callback(struct _lv_img_decoder_t * decoder, lv_image_decoder_dsc_t * dsc)
          *  \brief Callback to close the image source.
          *  \param decoder: pointer to the underlying image decoder instance.
          *  \param dsc: a pointer to an image decoder descriptor.
          *  \returns result code.
          */
-        virtual lv_res_t close_callback(struct _lv_img_decoder_t * decoder, struct _lv_img_decoder_dsc_t * dsc) = 0;
+        virtual lv_res_t close_callback(struct _lv_img_decoder_t * decoder, lv_image_decoder_dsc_t * dsc) = 0;
 
     public:
         using PointerWrapper::PointerWrapper;
-        /** \fn ImageDecoder(const ImageDescriptor & src, lv_color_t color, int32_t frame_id)
+
+        /** \fn ImageDecoder(const ImageDescriptor & src, const lv_image_decoder_args_t * args)
          *  \brief Constructor with image source.
          *  \param src: image descriptor.
-         *  \param color: background color.
-         *  \param frame_id: frame index (for animations).
+         *  \param args: decoder args
          */
-        ImageDecoder(const ImageDescriptor & src, lv_color_t color, int32_t frame_id);
-        /** \fn ImageDecoder(const std::string & src, lv_color_t color, int32_t frame_id)
-         *  \brief Constructor with image path.
-         *  \param src: image path.
-         *  \param color: background color.
-         *  \param frame_id: frame index (for animations).
-         */
-        ImageDecoder(const std::string & src, lv_color_t color, int32_t frame_id);
-
+        ImageDecoder(const ImageDescriptor & src, const lv_image_decoder_args_t * args);
 
         /** \fn static ImageHeader get_info(const ImageDescriptor & src)
          *  \brief Gets the image info.
@@ -109,6 +102,7 @@ namespace lvgl::draw {
          */
         lv_res_t open(const std::string & src, lv_color_t color, int32_t frame_id);
 
+#ifdef MISSING_PORT
         /** \fn lv_res_t read_line(lv_coord_t x, lv_coord_t y, lv_coord_t len, std::vector<uint8_t> & buf)
          *  \brief Reads one line from the opened image.
          *  \param x: horizontal coordinate.
@@ -118,6 +112,7 @@ namespace lvgl::draw {
          *  \returns result code.
          */
         lv_res_t read_line(lv_coord_t x, lv_coord_t y, lv_coord_t len, std::vector<uint8_t> & buf);
+#endif
 
         /** \fn void close()
          *  \brief Closes the image source.
@@ -127,9 +122,9 @@ namespace lvgl::draw {
     };
 
     /** \class ImageHeader
-     *  \brief Wraps a lv_img_header_t object.
+     *  \brief Wraps a lv_image_header_t object.
      */
-    class ImageHeader : public PointerWrapper<lv_img_header_t, lv_mem_free> {
+    class ImageHeader : public PointerWrapper<lv_image_header_t, lv_free> {
     private:
         /** \fn bool valid
          *  \brief True if header contains valid data, false otherwise.
@@ -154,21 +149,21 @@ namespace lvgl::draw {
 
 
     /** \class ImageDescriptor
-     *  \brief Wraps a lv_img_dsc_t object.
+     *  \brief Wraps a lv_image_decoder_dsc_t object.
      */
-    class ImageDescriptor : public PointerWrapper<lv_img_dsc_t, lv_img_buf_free> {
+    class ImageDescriptor : public PointerWrapper<lv_image_dsc_t, lv_image_buf_free> {
     public:
         using PointerWrapper::PointerWrapper;
 
         ImageDescriptor();
 
-        /** \fn ImageDescriptor(lv_coord_t w, lv_coord_t h, lv_img_cf_t cf)
+        /** \fn ImageDescriptor(lv_coord_t w, lv_coord_t h, lv_color_format_t cf)
          *  \brief Constructor with parameters.
          *  \param w: buffer width.
          *  \param h: buffer height.
          *  \param cf: buffer color format.
          */
-        ImageDescriptor(lv_coord_t w, lv_coord_t h, lv_img_cf_t cf);
+        ImageDescriptor(lv_coord_t w, lv_coord_t h, lv_color_format_t cf);
 
         /** \fn ImageDescriptor(const lv_img_dsc_t * dsc)
          *  \brief Construction with lv_img_dsc_t
@@ -176,6 +171,7 @@ namespace lvgl::draw {
          */
         ImageDescriptor(const lv_img_dsc_t * dsc);
 
+ #ifdef MISSING_PORT
         /** \fn lv_color_t get_px_color(lv_coord_t x, lv_coord_t y, lv_color_t bg_color) const
          *  \brief Gets pixel color.
          *  \param x: horizontal coordinate.
@@ -208,6 +204,7 @@ namespace lvgl::draw {
          *  \param opa: opacity value.
          */
         void set_px_alpha(lv_coord_t x, lv_coord_t y, lv_opa_t opa);
+#endif
 
         /** \fn void set_palette(uint8_t id, lv_color_t c)
          *  \brief Sets palette color.
@@ -215,7 +212,15 @@ namespace lvgl::draw {
          *  \param c: color.
          */
         void set_palette(uint8_t id, lv_color_t c);
-        
+
+        /** \fn void set_palette(uint8_t id, lv_color32_t c)
+         *  \brief Sets palette color.
+         *  \param id: color index.
+         *  \param c: color.
+         */
+        void set_palette(uint8_t id, lv_color32_t c);
+
+ #ifdef MISSING_PORT
         /** \fn uint32_t get_img_size(lv_coord_t w, lv_coord_t h, lv_img_cf_t cf) const
          *  \brief Computes image size in bytes.
          *  \param w: image width.
@@ -224,25 +229,19 @@ namespace lvgl::draw {
          *  \returns image size in bytes.
          */
         uint32_t get_img_size(lv_coord_t w, lv_coord_t h, lv_img_cf_t cf) const;
+#endif
 
-        /** \fn void set_src(const std::vector<uint8_t> & src, uint32_t w, uint32_t h, lv_img_cf_t cf)
+        /** \fn void set_src(const lv_image_dsc_t & dsc)
          *  \brief Sets image source.
-         *  \param src: source buffer.
-         *  \param w: image width.
-         *  \param h: image height.
-         *  \param cf: color format.
+         *  \param dsc: image descriptor
          */
-        void set_src(const std::vector<uint8_t> & src, uint32_t w, uint32_t h, lv_img_cf_t cf);
+        void set_src(const lv_image_dsc_t & dsc);
 
-        /** \fn void set_src(const uint8_t * src, uint32_t w, uint32_t h, lv_img_cf_t cf)
+        /** \fn void set_src(const lv_image_dsc_t * dsc)
          *  \brief Sets image source.
-         *  \param src: pointer to source buffer.
-         *  \param w: image width.
-         *  \param h: image height.
-         *  \param cf: color format.
+         *  \param dsc: image descriptor
          */
-        void set_src(const uint8_t * src, uint32_t w, uint32_t h, lv_img_cf_t cf);
-
+        void set_src(const lv_image_dsc_t * dsc);
     };
 
 }
