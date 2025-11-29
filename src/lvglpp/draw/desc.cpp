5c3f9df1f3a110386f9ce0274df32036ed46e0c3
Kim Bøndergaard <kim@fam-boendergaard.dk>
2025-11-29 17:55:51 +0100

    draw: desc: Port to LVGL 9.3
    class LetterDrawDescriptor added
    
    Argument list changed for most draw functions

--- src/lvglpp/draw/desc.cpp
+++ src/lvglpp/draw/desc.cpp
@@ -14,7 +14,7 @@ namespace lvgl::draw {
         lv_draw_rect_dsc_init(this->raw_ptr());
     }
 
-    void RectangleDrawDescriptor::draw(lv_draw_ctx_t * draw_ctx, const Area & coords) {
+    void RectangleDrawDescriptor::draw(lv_layer_t * draw_ctx, const Area & coords) {
         lv_draw_rect(draw_ctx, this->raw_ptr(), coords.raw_ptr());
     }
 
@@ -24,22 +24,27 @@ namespace lvgl::draw {
         lv_draw_label_dsc_init(this->raw_ptr());
     }
 
-    void LabelDrawDescriptor::draw(lv_draw_ctx_t * ctx, const Area & coords, const std::string & txt, lv_draw_label_hint_t * hint) {
-        lv_draw_label(ctx, this->raw_ptr(), coords.raw_ptr(), txt.c_str(), hint);
+    void LabelDrawDescriptor::draw(lv_layer_t * ctx, const Area & coords) {
+        lv_draw_label(ctx, this->raw_ptr(), coords.raw_ptr());
     }
 
-    void LabelDrawDescriptor::draw_letter(lv_draw_ctx_t * ctx, const lv_point_t & pos, uint32_t letter) {
-        lv_draw_letter(ctx, this->raw_ptr(), &pos, letter);
+    LetterDrawDescriptor::LetterDrawDescriptor() {
+        this->lv_obj = LvPointerType(lv_cls_alloc<lv_cls>());
+        lv_draw_letter_dsc_init(this->raw_ptr());
+    }
+
+    void LetterDrawDescriptor::draw_letter(lv_layer_t * ctx, const lv_point_t & pos) {
+        lv_draw_letter(ctx, this->raw_ptr(), &pos);
     }
 
 
     ImageDrawDescriptor::ImageDrawDescriptor() {
         this->lv_obj = LvPointerType(lv_cls_alloc<lv_cls>());
-        lv_draw_img_dsc_init(this->raw_ptr());
+        lv_draw_image_dsc_init(this->raw_ptr());
     }
 
-    void ImageDrawDescriptor::draw(lv_draw_ctx_t * ctx, const Area & coords, const ImageDescriptor & img) {
-        lv_draw_img(ctx, this->raw_ptr(), coords.raw_ptr(), img.raw_ptr());
+    void ImageDrawDescriptor::draw(lv_layer_t * ctx, const Area & coords) {
+        lv_draw_image(ctx, this->raw_ptr(), coords.raw_ptr());
     }
 
 
@@ -48,8 +53,8 @@ namespace lvgl::draw {
         lv_draw_line_dsc_init(this->raw_ptr());
     }
 
-    void LineDrawDescriptor::draw(lv_draw_ctx_t * ctx, const lv_point_t & point1, const lv_point_t & point2) {
-        lv_draw_line(ctx, this->raw_ptr(), &point1, &point2);
+    void LineDrawDescriptor::draw(lv_layer_t * ctx) {
+        lv_draw_line(ctx, this->raw_ptr());
     }
 
 
@@ -58,8 +63,8 @@ namespace lvgl::draw {
         lv_draw_arc_dsc_init(this->raw_ptr());
     }
 
-    void ArcDrawDescriptor::draw(lv_draw_ctx_t * ctx, const lv_point_t & center, uint16_t radius,  uint16_t start_angle, uint16_t end_angle) {
-        lv_draw_arc(ctx, this->raw_ptr(), &center, radius, start_angle, end_angle);
+    void ArcDrawDescriptor::draw(lv_layer_t * ctx) {
+        lv_draw_arc(ctx, this->raw_ptr());
     }
     
 }
