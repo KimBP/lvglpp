704dbe05d882545238fbd6a88023261f1aa0a69b
Kim Bøndergaard <kim@fam-boendergaard.dk>
2025-11-29 20:24:56 +0100

    widgets: imagebtn: Port to LVGL 9.3
    *btn* -> *BUTTON*

--- src/lvglpp/widgets/imgbtn/imgbtn.cpp
+++ src/lvglpp/widgets/imgbtn/imgbtn.cpp
@@ -1,115 +1,132 @@
 /** \file imgbtn.cpp
- *  \brief Implementation file for C++ wrapper for lv_imgbtn_t objects.
+ *  \brief Implementation file for C++ wrapper for lv_imagebutton_t objects.
  *
  *  Author: Vincent Paeder
  *  License: MIT
  */
-#include "imgbtn.h"
-#if LV_USE_IMGBTN != 0
+
+ #include "src/widgets/imagebutton/lv_imagebutton_private.h"
+ #include "imgbtn.h"
+#if LV_USE_IMAGEBUTTON != 0
 
 #include "../../draw/image.h"
 
 namespace lvgl::widgets {
 
-    void ImageButton::set_src_left_img(lv_imgbtn_state_t state, const ImageDescriptor & src) {
-        reinterpret_cast<lv_imgbtn_t*>(this->raw_ptr())->img_src_left[state] = src.raw_ptr();
+    void ImageButton::set_src_left_img(lv_imagebutton_state_t state, const ImageDescriptor & src) {
+        lv_imagebutton_src_info_t* info = &reinterpret_cast<lv_imagebutton_t*>(this->raw_ptr())->src_left[state];
+
+        (void)lv_image_decoder_get_info(src.raw_ptr(), &info->header);
+        info->img_src = src.raw_ptr();
     }
 
-    void ImageButton::set_src_left_str(lv_imgbtn_state_t state, const std::string & src) {
-        reinterpret_cast<lv_imgbtn_t*>(this->raw_ptr())->img_src_left[state] = src.c_str();
+    void ImageButton::set_src_left_str(lv_imagebutton_state_t state, const std::string & src) {
+        lv_imagebutton_src_info_t* info = &reinterpret_cast<lv_imagebutton_t*>(this->raw_ptr())->src_left[state];
+        (void)lv_image_decoder_get_info(src.c_str(), &info->header);
+        info->img_src = src.c_str();
     }
 
-    void ImageButton::set_src_mid_img(lv_imgbtn_state_t state, const ImageDescriptor & src) {
-        reinterpret_cast<lv_imgbtn_t*>(this->raw_ptr())->img_src_mid[state] = src.raw_ptr();
+    void ImageButton::set_src_mid_img(lv_imagebutton_state_t state, const ImageDescriptor & src) {
+        lv_imagebutton_src_info_t* info = &reinterpret_cast<lv_imagebutton_t*>(this->raw_ptr())->src_mid[state];
+
+        (void)lv_image_decoder_get_info(src.raw_ptr(), &info->header);
+        info->img_src = src.raw_ptr();
     }
 
-    void ImageButton::set_src_mid_str(lv_imgbtn_state_t state, const std::string & src) {
-        reinterpret_cast<lv_imgbtn_t*>(this->raw_ptr())->img_src_mid[state] = src.c_str();
+    void ImageButton::set_src_mid_str(lv_imagebutton_state_t state, const std::string & src) {
+        lv_imagebutton_src_info_t* info = &reinterpret_cast<lv_imagebutton_t*>(this->raw_ptr())->src_mid[state];
+        (void)lv_image_decoder_get_info(src.c_str(), &info->header);
+        info->img_src = src.c_str();
     }
 
-    void ImageButton::set_src_right_img(lv_imgbtn_state_t state, const ImageDescriptor & src) {
-        reinterpret_cast<lv_imgbtn_t*>(this->raw_ptr())->img_src_right[state] = src.raw_ptr();
+    void ImageButton::set_src_right_img(lv_imagebutton_state_t state, const ImageDescriptor & src) {
+        lv_imagebutton_src_info_t* info = &reinterpret_cast<lv_imagebutton_t*>(this->raw_ptr())->src_right[state];
+
+        (void)lv_image_decoder_get_info(src.raw_ptr(), &info->header);
+        info->img_src = src.raw_ptr();
     }
 
-    void ImageButton::set_src_right_str(lv_imgbtn_state_t state, const std::string & src) {
-        reinterpret_cast<lv_imgbtn_t*>(this->raw_ptr())->img_src_right[state] = src.c_str();
+    void ImageButton::set_src_right_str(lv_imagebutton_state_t state, const std::string & src) {
+        lv_imagebutton_src_info_t* info = &reinterpret_cast<lv_imagebutton_t*>(this->raw_ptr())->src_right[state];
+        (void)lv_image_decoder_get_info(src.c_str(), &info->header);
+        info->img_src = src.c_str();
     }
 
-    void ImageButton::set_state(lv_imgbtn_state_t state) {
+    void ImageButton::set_state(lv_imagebutton_state_t state) {
         lv_imgbtn_set_state(this->raw_ptr(), state);
     }
 
-    lv_img_src_t ImageButton::get_src_left_type(lv_imgbtn_state_t state) const {
-        return lv_img_src_get_type(
-            reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_left[state]);
+    lv_image_src_t ImageButton::get_src_left_type(lv_imagebutton_state_t state) const {
+        return lv_image_src_get_type(
+            &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_left[state]);
     }
 
-    lv_img_src_t ImageButton::get_src_mid_type(lv_imgbtn_state_t state) const {
-        return lv_img_src_get_type(
-            reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_mid[state]);
+    lv_image_src_t ImageButton::get_src_mid_type(lv_imagebutton_state_t state) const {
+        return lv_image_src_get_type(
+            &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_mid[state]);
     }
 
-    lv_img_src_t ImageButton::get_src_right_type(lv_imgbtn_state_t state) const {
-        return lv_img_src_get_type(
-            reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_right[state]);
+    lv_image_src_t ImageButton::get_src_right_type(lv_imagebutton_state_t state) const {
+        return lv_image_src_get_type(
+            &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_right[state]);
     }
 
-    const ImageDescriptor ImageButton::get_src_left_img(lv_imgbtn_state_t state) const {
-        assert(this->get_src_left_type(state) == LV_IMG_SRC_VARIABLE);
+    const ImageDescriptor ImageButton::get_src_left_img(lv_imagebutton_state_t state) const {
+        assert(this->get_src_left_type(state) == LV_IMAGE_SRC_VARIABLE);
         return ImageDescriptor(
             const_cast<lv_img_dsc_t*>(reinterpret_cast<const lv_img_dsc_t*>(
-                reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_left[state]
+                &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_left[state]
             )
         ));
     }
 
-    const std::string ImageButton::get_src_left_str(lv_imgbtn_state_t state) const {
+    const std::string ImageButton::get_src_left_str(lv_imagebutton_state_t state) const {
         auto src_type = this->get_src_left_type(state);
-        assert(src_type == LV_IMG_SRC_FILE || src_type == LV_IMG_SRC_SYMBOL);
+        assert(src_type == LV_IMAGE_SRC_FILE || src_type == LV_IMAGE_SRC_SYMBOL);
         return std::string(
             const_cast<char*>(reinterpret_cast<const char*>(
-                reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_left[state]
+                &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_left[state]
             )
         ));
     }
 
-    const ImageDescriptor ImageButton::get_src_mid_img(lv_imgbtn_state_t state) const {
-        assert(this->get_src_mid_type(state) == LV_IMG_SRC_VARIABLE);
+    const ImageDescriptor ImageButton::get_src_mid_img(lv_imagebutton_state_t state) const {
+        assert(this->get_src_mid_type(state) == LV_IMAGE_SRC_VARIABLE);
         return ImageDescriptor(
             const_cast<lv_img_dsc_t*>(reinterpret_cast<const lv_img_dsc_t*>(
-                reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_mid[state]
+                &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_mid[state]
             )
         ));
     }
 
-    const std::string ImageButton::get_src_mid_str(lv_imgbtn_state_t state) const {
+    const std::string ImageButton::get_src_mid_str(lv_imagebutton_state_t state) const {
         auto src_type = this->get_src_mid_type(state);
-        assert(src_type == LV_IMG_SRC_FILE || src_type == LV_IMG_SRC_SYMBOL);
+        assert(src_type == LV_IMAGE_SRC_FILE || src_type == LV_IMAGE_SRC_SYMBOL);
         return std::string(
             const_cast<char*>(reinterpret_cast<const char*>(
-                reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_mid[state]
+                &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_mid[state]
             )
         ));
     }
 
-    const ImageDescriptor ImageButton::get_src_right_img(lv_imgbtn_state_t state) const {
-        assert(this->get_src_right_type(state) == LV_IMG_SRC_VARIABLE);
+    const ImageDescriptor ImageButton::get_src_right_img(lv_imagebutton_state_t state) const {
+        assert(this->get_src_right_type(state) == LV_IMAGE_SRC_VARIABLE);
         return ImageDescriptor(
             const_cast<lv_img_dsc_t*>(reinterpret_cast<const lv_img_dsc_t*>(
-                reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_right[state]
+                &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_right[state]
             )
         ));
     }
 
-    const std::string ImageButton::get_src_right_str(lv_imgbtn_state_t state) const {
+    const std::string ImageButton::get_src_right_str(lv_imagebutton_state_t state) const {
         auto src_type = this->get_src_right_type(state);
-        assert(src_type == LV_IMG_SRC_FILE || src_type == LV_IMG_SRC_SYMBOL);
+        assert(src_type == LV_IMAGE_SRC_FILE || src_type == LV_IMAGE_SRC_SYMBOL);
         return std::string(
-            const_cast<char*>(reinterpret_cast<const char*>(
-                reinterpret_cast<const lv_imgbtn_t*>(this->raw_ptr())->img_src_right[state]
-            )
+             const_cast<char*>(reinterpret_cast<const char*>(
+                 &reinterpret_cast<const lv_imagebutton_t*>(this->raw_ptr())->src_right[state]
+             )
         ));
     }
 
 }
-#endif // LV_USE_IMGBTN
+#endif // LV_USE_IMAGEBUTTON
