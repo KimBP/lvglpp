00a7208dbed0614126f635ee76673a0abd2ec75f
Kim Bøndergaard <kim@fam-boendergaard.dk>
2025-11-30 10:25:49 +0100

    widgets: spinner: Port to LVGL 9.3
    New constructors added

--- src/lvglpp/widgets/spinner/spinner.cpp
+++ src/lvglpp/widgets/spinner/spinner.cpp
@@ -9,21 +9,27 @@
 
 namespace lvgl::widgets {
 
+    Spinner::Spinner() {
+        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_spinner_create(nullptr));
+    }
+
+    Spinner::Spinner(Object & parent) {
+        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_spinner_create(parent.raw_ptr()));
+    }
+
     Spinner::Spinner(uint32_t time, uint32_t arc_length) {
-        this->initialize(time, arc_length);
+        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_spinner_create(nullptr));
+        set_anim_params(time, arc_length);
     }
 
     Spinner::Spinner(Object & parent, uint32_t time, uint32_t arc_length) {
-        this->initialize(parent, time, arc_length);
+        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_spinner_create(parent.raw_ptr()));
+        set_anim_params(time, arc_length);
     }
 
-    void Spinner::initialize(uint32_t time, uint32_t arc_length) {
-        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_spinner_create(nullptr, time, arc_length));
+    void Spinner::set_anim_params(uint32_t time, uint32_t arc_length)
+    {
+        lv_spinner_set_anim_params(this->raw_ptr(), time, arc_length);
     }
-
-    void Spinner::initialize(Object & parent, uint32_t time, uint32_t arc_length) {
-        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_spinner_create(parent.raw_ptr(), time, arc_length));
-    }
-
 }
 #endif // LV_USE_SPINNER
--- src/lvglpp/widgets/spinner/spinner.h
+++ src/lvglpp/widgets/spinner/spinner.h
@@ -18,28 +18,20 @@ namespace lvgl::widgets {
      *  \brief Wraps a lv_spinner_t object.
      */
     class Spinner : public Object {
-    protected:
-        void initialize() = delete;
-        void initialize(Object & parent) = delete;
-        /** \fn void initialize(uint32_t time, uint32_t arc_length)
-         *  \brief Initialize object with parameters.
-         *  \param time: duration of spinner animation, in ms.
-         *  \param arc_length: length of spinner arc, in degrees.
-         */
-        void initialize(uint32_t time, uint32_t arc_length);
-        /** \fn void initialize(Object & parent, uint32_t time, uint32_t arc_length)
-         *  \brief Initialize object with parameters and parent object.
-         *  \param parent: parent LVGL object.
-         *  \param time: duration of spinner animation, in ms.
-         *  \param arc_length: length of spinner arc, in degrees.
-         */
-        void initialize(Object & parent, uint32_t time, uint32_t arc_length);
-    
     public:
         using Object::Object;
         
-        Spinner() = delete;
-        Spinner(Object & parent) = delete;
+        /** \fn Spinner()
+         *  \brief Default constructor
+         */
+        Spinner();
+
+        /** \fn Spinner(Object & parent)
+         *  \brief Constructor with parent object.
+         *  \param parent: parent LVGL object.
+         */
+        Spinner(Object & parent);
+
         /** \fn Spinner(uint32_t time, uint32_t arc_length)
          *  \brief Constructor with parameters.
          *  \param time: duration of spinner animation, in ms.
@@ -53,6 +45,13 @@ namespace lvgl::widgets {
          *  \param arc_length: length of spinner arc, in degrees.
          */
         Spinner(Object & parent, uint32_t time, uint32_t arc_length);
+
+        /** \fn void set_anim_params(uint32_t time, uint32_t arc_length)
+         *  \brief Configure animation parametes
+         *  \param time: duration of spinner animation, in ms.
+         *  \param arc_length: length of spinner arc, in degrees.
+         */
+        void set_anim_params(uint32_t time, uint32_t arc_length);
     };
 
 }
