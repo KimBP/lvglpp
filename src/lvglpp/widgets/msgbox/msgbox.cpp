099c484ebade240d9f7b378c7b1444d4fa025271
Kim Bøndergaard <kim@fam-boendergaard.dk>
2025-11-30 10:19:41 +0100

    widgets: msgbox: Port to LVGL 9.3
    New Constructors added.
    
    New utilities for 'building' the msgbox added

--- src/lvglpp/widgets/msgbox/msgbox.cpp
+++ src/lvglpp/widgets/msgbox/msgbox.cpp
@@ -9,73 +9,72 @@
 
 namespace lvgl::widgets {
 
+    MessageBox::MessageBox(Object & parent) {
+        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(lv_msgbox_create(nullptr));
+    }
+
     MessageBox::MessageBox(const std::string & title, const std::string & txt,
                            const std::vector<std::string> & btn_txts,
-                           bool add_close_btn) {
+                           bool add_close_btn)
+                           : MessageBox()
+                           {
         this->initialize(title, txt, btn_txts, add_close_btn);
     }
 
     MessageBox::MessageBox(Object & parent, const std::string & title, const std::string & txt,
                            const std::vector<std::string> & btn_txts,
-                           bool add_close_btn) {
-        this->initialize(parent, title, txt, btn_txts, add_close_btn);
+                           bool add_close_btn)
+                           : MessageBox(parent)
+                           {
+        this->initialize(title, txt, btn_txts, add_close_btn);
     }
 
     void MessageBox::initialize(const std::string & title, const std::string & txt,
                                 const std::vector<std::string> & btn_txts,
                                 bool add_close_btn) {
-        const char ** btxts = nullptr;
-        if (btn_txts.size()>0) {
-            this->btn_txts = str_vector_to_char_array(btn_txts);
-            btxts = (const char**)this->btn_txts.get(); // see ButtonMatrix::set_map for explanation
+        add_title(title);
+        add_text(txt);
+
+        for(auto & btn_txt : btn_txts) {
+            add_footer_button(btn_txt);
         }
-        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(
-            lv_msgbox_create(nullptr,
-                             title.c_str(),
-                             txt.c_str(),
-                             btxts,
-                             add_close_btn)
-        );
-        
+
+        if (add_close_btn) add_close_button();
     }
 
-    void MessageBox::initialize(Object & parent, const std::string & title, const std::string & txt,
-                                const std::vector<std::string> & btn_txts,
-                                bool add_close_btn) {
-        const char ** btxts = nullptr;
-        if (btn_txts.size()>0) {
-            this->btn_txts = str_vector_to_char_array(btn_txts);
-            btxts = (const char**)this->btn_txts.get(); // see ButtonMatrix::set_map for explanation
-        }
-        this->lv_obj = LvPointer<lv_obj_t, lv_obj_del>(
-            lv_msgbox_create(parent.raw_ptr(),
-                             title.c_str(),
-                             txt.c_str(),
-                             btxts,
-                             add_close_btn)
-        );
+    Object MessageBox::add_title(const std::string & title) {
+        return Object(lv_msgbox_add_title(this->raw_ptr(), title.c_str()));
+    }
+
+    Object MessageBox::add_header_button(const void* icon) {
+        return Object(lv_msgbox_add_header_button(this->raw_ptr(), icon));
+    }
+
+    Object MessageBox::add_text(const std::string & text) {
+        return Object(lv_msgbox_add_text(this->raw_ptr(), text.c_str()));
+    }
+
+    Object MessageBox::add_footer_button(const std::string & text) {
+        return Object(lv_msgbox_add_footer_button(this->raw_ptr(), text.c_str()));
+    }
+
+    Object MessageBox::add_close_button() {
+        return Object(lv_msgbox_add_close_button(this->raw_ptr()));
     }
 
     Object MessageBox::get_title() const {
         return Object(lv_msgbox_get_title(const_cast<lv_cls_ptr>(this->raw_ptr())));
     }
 
-    Object MessageBox::get_close_btn() const {
-        return Object(lv_msgbox_get_close_btn(const_cast<lv_cls_ptr>(this->raw_ptr())));
-    }
-
-    Object MessageBox::get_text() const {
-        return Object(lv_msgbox_get_text(const_cast<lv_cls_ptr>(this->raw_ptr())));
-    }
-
     Object MessageBox::get_content() const {
         return Object(lv_msgbox_get_content(const_cast<lv_cls_ptr>(this->raw_ptr())));
     }
 
-    Object MessageBox::get_btns() const {
-        return Object(lv_msgbox_get_btns(const_cast<lv_cls_ptr>(this->raw_ptr())));
+    Object MessageBox::get_footer() const {
+        return Object(lv_msgbox_get_footer(const_cast<lv_cls_ptr>(this->raw_ptr())));
     }
 
+#ifdef MISSING_PORT
     uint16_t MessageBox::get_active_btn() const {
         return lv_msgbox_get_active_btn(const_cast<lv_cls_ptr>(this->raw_ptr()));
     }
@@ -83,6 +82,7 @@ namespace lvgl::widgets {
     std::string MessageBox::get_active_btn_text() const {
         return std::string(lv_msgbox_get_active_btn_text(const_cast<lv_cls_ptr>(this->raw_ptr())));
     }
+#endif
 
     void MessageBox::close() {
         lv_msgbox_close(this->raw_ptr());
--- src/lvglpp/widgets/msgbox/msgbox.h
+++ src/lvglpp/widgets/msgbox/msgbox.h
@@ -39,25 +39,21 @@ namespace lvgl::widgets {
         void initialize(const std::string & title, const std::string & txt,
                         const std::vector<std::string> & btn_txts,
                         bool add_close_btn);
-        /** \fn void initialize(Object & parent, const std::string & title,
-         *                      const std::string & txt, const std::vector<std::string> & btn_txts,
-         *                      bool add_close_btn)
-         *  \brief Initialize object and assign parent object.
-         *  \param parent: parent LVGL object.
-         *  \param title: message box title.
-         *  \param txt: message box text.
-         *  \param btn_txts: button texts.
-         *  \param add_close_btn: if true, adds a close button.
-         */
-        void initialize(Object & parent, const std::string & title, const std::string & txt,
-                        const std::vector<std::string> & btn_txts,
-                        bool add_close_btn);
     
     public:
         using Object::Object;
-        
-        MessageBox() = delete;
-        MessageBox(Object & parent) = delete;
+
+        /** \fn MessageBox()
+         *  \brief Default constructor
+         */
+        MessageBox() : MessageBox(nullptr) {};
+
+        /** \fn MessageBox(Object & parent)
+         *  \brief Consructor with parent object.
+         *  \param parent: parent LVGL object.
+         */
+        MessageBox(Object & parent);
+
         /** \fn MessageBox(const std::string & title, const std::string & txt,
          *                 const std::vector<std::string> & btn_txts,
          *                 bool add_close_btn)
@@ -70,6 +66,7 @@ namespace lvgl::widgets {
         MessageBox(const std::string & title, const std::string & txt,
                    const std::vector<std::string> & btn_txts,
                    bool add_close_btn);
+
         /** \fn MessageBox(Object & parent, const std::string & title,
          *                 const std::string & txt, const std::vector<std::string> & btn_txts,
          *                 bool add_close_btn)
@@ -84,12 +81,47 @@ namespace lvgl::widgets {
                    const std::vector<std::string> & btn_txts,
                    bool add_close_btn);
 
+        /** \fn Object add_title(const std::string & title)
+         *  \brief Add title to messagebox
+         *  \param title: message box title
+         *  \returns object for title element
+         */
+        Object add_title(const std::string & title);
+
+        /** \fn Object add_header_button(const void* icon)
+         *  \brief Add a button to header
+         *  \param icon: The icon identifying the button
+         *  \returns object for button element
+         */
+        Object add_header_button(const void* icon);
+
+        /** \fn Object add_text(const std::string & text)
+         *  \brief Adds message box text
+         *  \param text: message box text
+         *  \returns object for text element
+         */
+        Object add_text(const std::string & text);
+
+        /** \fn Object add_footer_button(const std::string & text)
+         *  \brief Adds message box footer button
+         *  \param text: button text
+         *  \returns object for text element
+         */
+        Object add_footer_button(const std::string & text);
+
+        /** \fn Object add_close_button()
+         *  \brief Adds close button in header
+         *  \returns object for button
+         */
+        Object add_close_button();
+
         /** \fn Object get_title() const
          *  \brief Gets object for title element.
          *  \returns object for title element.
          */
         Object get_title() const;
 
+#ifdef MISSING_PORT
         /** \fn Object get_close_btn() const
          *  \brief Gets object for close button.
          *  \returns object for close button.
@@ -101,19 +133,33 @@ namespace lvgl::widgets {
          *  \returns object for text.
          */
         Object get_text() const;
+#endif
 
         /** \fn Object get_content() const
          *  \brief Gets object for content.
          *  \returns object for content.
+         *
+         *   Content contains text label and footer
          */
         Object get_content() const;
 
+        /** \fn Object get_footer() const
+         *  \brief Gets object for footer.
+         *  \returns object for footer.
+         *
+         *   footer contains buttons
+         */
+        Object get_footer() const;
+
         /** \fn Object get_btns() const
          *  \brief Gets object for buttons.
-         *  \returns object for buttons.
+         *  \returns object for buttons (aka footer)
+         *
+         *  Function deprecated
          */
-        Object get_btns() const;
+        Object get_btns() const { return get_footer(); };
 
+ #ifdef MISSING_PORT
         /** \fn uint16_t get_active_btn() const
          *  \brief Gets active button.
          *  \returns active button index.
@@ -125,6 +171,7 @@ namespace lvgl::widgets {
          *  \returns active button text string.
          */
         std::string get_active_btn_text() const;
+#endif
 
         /** \fn void close()
          *  \brief Closes message box.
